6edf2b1207845be054553461491df9188a4bd0fd
saera <yook.saera@gmail.com>
2025-09-22 17:37:12 +0900

    feat: 지하철 비인기 노선 초기화 기능 추가

--- backend/src/main/java/com/f12/moitz/infrastructure/client/open/dto/utils/StationLineNameConverter.java
+++ backend/src/main/java/com/f12/moitz/infrastructure/client/open/dto/utils/StationLineNameConverter.java
@@ -9,7 +9,12 @@ import java.util.Map;
 public class StationLineNameConverter extends JsonDeserializer<String> {
 
     private static final Map<String, String> NAME_EXCEPTIONS = Map.of(
-            "경의선", "경의중앙선"
+            "경의선", "경의중앙선",
+            "경의선 지선", "경의중앙선",
+            "우이신설경전철", "우이신설선",
+            "김포도시철도", "김포골드라인",
+            "용인경전철", "용인에버라인",
+            "인천선", "인천1호선"
     );
 
     @Override
