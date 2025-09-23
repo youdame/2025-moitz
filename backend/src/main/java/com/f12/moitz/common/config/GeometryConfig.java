30d7992e061d8a7187929686ab7ee774933065b8
DEV_WOONG <ywcsuwon@gmail.com>
2025-09-23 10:24:51 +0900

    feat: GeometryConfig 클래스 구현
    - GeometryFactory 빈 생성

---
+++ backend/src/main/java/com/f12/moitz/common/config/GeometryConfig.java
@@ -0,0 +1,14 @@
+package com.f12.moitz.common.config;
+
+import org.locationtech.jts.geom.GeometryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeometryConfig {
+
+    @Bean
+    public GeometryFactory geometryFactory() {
+        return new GeometryFactory();
+    }
+}
