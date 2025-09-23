745faa626221ac826dee2df3f1805ec8d940d9a2
saera <yook.saera@gmail.com>
2025-09-23 22:28:06 +0900

    fix: 외부 API 및 DB 의존성으로 실패하는 테스트 수정

--- backend/src/test/java/com/f12/moitz/MoitzApplicationTests.java
+++ backend/src/test/java/com/f12/moitz/MoitzApplicationTests.java
@@ -1,13 +1,20 @@
 package com.f12.moitz;
 
+import com.f12.moitz.application.SetupService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class MoitzApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean
+    private SetupService setupService;
+
+    @Test
+    void contextLoads() {
+        Mockito.doNothing().when(setupService).setup();
+    }
 
 }
